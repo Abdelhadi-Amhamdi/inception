402e5973143ea2c370b0e84016f89d3e03fac67d
aamhamdi <aamhamdi943@gmail.com>
2024-03-02 18:19:16 +0100

    [fix issues]

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sleep 10
+wp config create --dbname=wordpress --dbuser=myuser --dbpass=root --dbhost=mariadb --allow-root
+# wp core install --url="localhost/test" --title="test" --admin_user="myuser" --admin_password="root" --admin_email="example@gmail.com" --allow-root
 mkdir -p /var/www/aamhamdi/html
-
 cp -r * /var/www/aamhamdi/html
 
 php-fpm8.2 -F -O
